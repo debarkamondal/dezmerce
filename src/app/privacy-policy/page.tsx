794b4cac55561d2df0b9f3e2815cbdc640e8df21
debarka <debarkamondal@gmail.com>
2025-05-19 12:19:06 +0530

    fix: build linter errors

--- src/app/privacy-policy/page.tsx
+++ src/app/privacy-policy/page.tsx
@@ -4,9 +4,9 @@ const PrivacyPolicyPage = () => {
       <h1 className="my-8 text-4xl font-semibold">Privacy Policy</h1>
       <p>
         This is a website to add to my portfolio. I only store your email
-        address and don't share it with anyone. Neither will I ever spam your
-        email. It's a promise. Just don't sign up if your paranoid you can
-        explore without signing up.
+        address and don&apos;t share it with anyone. Neither will I ever spam
+        your email. It&apos;s a promise. Just don&apos;t sign up if your
+        paranoid you can explore without signing up.
       </p>
     </>
   );
--- src/app/terms-and-conditions/page.tsx
+++ src/app/terms-and-conditions/page.tsx
@@ -5,7 +5,7 @@ const TermsAndConditionsPage = () => {
       <p>
         This is a website to add to my portfolio. There are no Terms &
         Condidions. This website is for demonstration purposes (demonstration of
-        my skills as a devloeper) please don't make any payments using your
+        my skills as a devloeper) please don&apos;t make any payments using your
         credentials use Test credentials provided.
       </p>
     </>
