fb454aaf478581431565e68f9a5609731852d110
debarka <debarkamondal@gmail.com>
2025-05-19 13:47:30 +0530

    fix: created pages for razorpay integration

--- src/app/cancellation-and-refund/page.tsx
+++ src/app/cancellation-and-refund/page.tsx
@@ -2,30 +2,177 @@ const CancellationAndRefund = () => {
   return (
     <>
       <h1 className="my-8 text-4xl font-semibold">Cancellation and Refund</h1>
+      <p>Last updated: May 19, 2025</p>
+      <p>Thank you for shopping at Debarka Modal.</p>
       <p>
-        Last updated on May 19 2025 Debarka Mondal believes in helping its
-        customers as far as possible, and has therefore a liberal cancellation
-        policy. Under this policy: Cancellations will be considered only if the
-        request is made within 2-3 days of placing the order. However, the
-        cancellation request may not be entertained if the orders have been
-        communicated to the vendors/merchants and they have initiated the
-        process of shipping them. Debarka Mondal does not accept cancellation
-        requests for perishable items like flowers, eatables etc. However,
-        refund/replacement can be made if the customer establishes that the
-        quality of product delivered is not good. In case of receipt of damaged
-        or defective items please report the same to our Customer Service team.
-        The request will, however, be entertained once the merchant has checked
-        and determined the same at his own end. This should be reported within
-        2-3 days of receipt of the products. In case you feel that the product
-        received is not as shown on the site or as per your expectations, you
-        must bring it to the notice of our customer service within 2-3 days of
-        receiving the product. The Customer Service Team after looking into your
-        complaint will take an appropriate decision. In case of complaints
-        regarding products that come with a warranty from manufacturers, please
-        refer the issue to them. In case of any Refunds approved by the Debarka
-        Mondal, it’ll take 2-3 days for the refund to be processed to the end
-        customer.{" "}
+        If, for any reason, You are not completely satisfied with a purchase We
+        invite You to review our policy on refunds and returns. This Return and
+        Refund Policy has been created with the help of the{" "}
+        <a
+          href="https://www.privacypolicies.com/return-refund-policy-generator/"
+          target="_blank"
+        >
+          Return and Refund Policy Generator
+        </a>
+        .
       </p>
+      <p>
+        The following terms are applicable for any products that You purchased
+        with Us.
+      </p>
+      <h2>Interpretation and Definitions</h2>
+      <h3>Interpretation</h3>
+      <p>
+        The words of which the initial letter is capitalized have meanings
+        defined under the following conditions. The following definitions shall
+        have the same meaning regardless of whether they appear in singular or
+        in plural.
+      </p>
+      <h3>Definitions</h3>
+      <p>For the purposes of this Return and Refund Policy:</p>
+      <ul>
+        <li>
+          <p>
+            <strong>Company</strong> (referred to as either &quot;the
+            Company&quot;, &quot;We&quot;, &quot;Us&quot; or &quot;Our&quot; in
+            this Agreement) refers to Debarka Modal.
+          </p>
+        </li>
+        <li>
+          <p>
+            <strong>Goods</strong> refer to the items offered for sale on the
+            Service.
+          </p>
+        </li>
+        <li>
+          <p>
+            <strong>Orders</strong> mean a request by You to purchase Goods from
+            Us.
+          </p>
+        </li>
+        <li>
+          <p>
+            <strong>Service</strong> refers to the Website.
+          </p>
+        </li>
+        <li>
+          <p>
+            <strong>Website</strong> refers to Debarka Modal, accessible from{" "}
+            <a
+              href="https://www.dkmondal.in"
+              rel="external nofollow noopener"
+              target="_blank"
+            >
+              https://www.dkmondal.in
+            </a>
+          </p>
+        </li>
+        <li>
+          <p>
+            <strong>You</strong> means the individual accessing or using the
+            Service, or the company, or other legal entity on behalf of which
+            such individual is accessing or using the Service, as applicable.
+          </p>
+        </li>
+      </ul>
+      <h2>Your Order Cancellation Rights</h2>
+      <p>
+        You are entitled to cancel Your Order within 30 days without giving any
+        reason for doing so.
+      </p>
+      <p>
+        The deadline for cancelling an Order is 30 days from the date on which
+        You received the Goods or on which a third party you have appointed, who
+        is not the carrier, takes possession of the product delivered.
+      </p>
+      <p>
+        In order to exercise Your right of cancellation, You must inform Us of
+        your decision by means of a clear statement. You can inform us of your
+        decision by:
+      </p>
+      <ul>
+        <li>By email: debarkamondal@gmail.com</li>
+      </ul>
+      <p>
+        We will reimburse You no later than 14 days from the day on which We
+        receive the returned Goods. We will use the same means of payment as You
+        used for the Order, and You will not incur any fees for such
+        reimbursement.
+      </p>
+      <h2>Conditions for Returns</h2>
+      <p>
+        In order for the Goods to be eligible for a return, please make sure
+        that:
+      </p>
+      <ul>
+        <li>The Goods were purchased in the last 30 days</li>
+        <li>The Goods are in the original packaging</li>
+      </ul>
+      <p>The following Goods cannot be returned:</p>
+      <ul>
+        <li>
+          The supply of Goods made to Your specifications or clearly
+          personalized.
+        </li>
+        <li>
+          The supply of Goods which according to their nature are not suitable
+          to be returned, deteriorate rapidly or where the date of expiry is
+          over.
+        </li>
+        <li>
+          The supply of Goods which are not suitable for return due to health
+          protection or hygiene reasons and were unsealed after delivery.
+        </li>
+        <li>
+          The supply of Goods which are, after delivery, according to their
+          nature, inseparably mixed with other items.
+        </li>
+      </ul>
+      <p>
+        We reserve the right to refuse returns of any merchandise that does not
+        meet the above return conditions in our sole discretion.
+      </p>
+      <p>
+        Only regular priced Goods may be refunded. Unfortunately, Goods on sale
+        cannot be refunded. This exclusion may not apply to You if it is not
+        permitted by applicable law.
+      </p>
+      <h2>Returning Goods</h2>
+      <p>
+        You are responsible for the cost and risk of returning the Goods to Us.
+        You should send the Goods at the following address:
+      </p>
+      <p>
+        72, Test stree,
+        <br />
+        Test Area 12, WB:741235
+      </p>
+      <p>
+        We cannot be held responsible for Goods damaged or lost in return
+        shipment. Therefore, We recommend an insured and trackable mail service.
+        We are unable to issue a refund without actual receipt of the Goods or
+        proof of received return delivery.
+      </p>
+      <h2>Gifts</h2>
+      <p>
+        If the Goods were marked as a gift when purchased and then shipped
+        directly to you, You&quot;ll receive a gift credit for the value of your
+        return. Once the returned product is received, a gift certificate will
+        be mailed to You.
+      </p>
+      <p>
+        If the Goods weren&quot;t marked as a gift when purchased, or the gift
+        giver had the Order shipped to themselves to give it to You later, We
+        will send the refund to the gift giver.
+      </p>
+      <h3>Contact Us</h3>
+      <p>
+        If you have any questions about our Returns and Refunds Policy, please
+        contact us:
+      </p>
+      <ul>
+        <li>By email: debarkamondal@gmail.com</li>
+      </ul>{" "}
     </>
   );
 };
--- src/app/shipping-and-delivery/page.tsx
+++ src/app/shipping-and-delivery/page.tsx
@@ -1,25 +1,117 @@
 const ShippingAndDelivery = () => {
   return (
     <>
-      <h1 className="text-3xl font-semibold">Shipping and Delivery</h1>
-      <p>This website is just for demonstration purposes for my portfolio</p>
+      <h1 className="my-8 text-3xl font-semibold">Shipping and Delivery</h1>
+      <h1>Shipping policy</h1>
       <p>
-        Last updated on May 19 2025 For International buyers, orders are shipped
-        and delivered through registered international courier companies and/or
-        International speed post only. For domestic buyers, orders are shipped
-        through registered domestic courier companies and /or speed post only.
-        Orders are shipped within 0-7 days or as per the delivery date agreed at
-        the time of order confirmation and delivering of the shipment subject to
-        Courier Company / post office norms. Debarka Mondal is not liable for
-        any delay in delivery by the courier company / postal authorities and
-        only guarantees to hand over the consignment to the courier company or
-        postal authorities within 0-7 days rom the date of the order and payment
-        or as per the delivery date agreed at the time of order confirmation.
-        Delivery of all orders will be to the address provided by the buyer.
-        Delivery of our services will be confirmed on your mail ID as specified
-        during registration. For any issues in utilizing our services you may
-        contact our helpdesk on debarkamondal@gmail.com
+        This shipping policy explains how Debarka Mondal (doing business as
+        “Dezire”) operates its shipping procedures and how we strive to meet
+        your expectations with every order. Whether you’re a first-time buyer or
+        a returning customer, we want to ensure that your experience with us is
+        smooth and satisfactory, right from placing your order to the moment it
+        arrives at your doorstep. This policy has been created with the help of
+        the{" "}
+        <a href="https://www.websitepolicies.com/shipping-policy-generator">
+          shipping policy generator
+        </a>
+        .
       </p>
+      <p>
+        Please read this shipping policy together with our{" "}
+        <a href="https://www.dkmondal.in/terms-and-conditions">
+          terms and conditions
+        </a>{" "}
+        to familiarize yourself with the rest of our general guidelines.
+      </p>
+      <div className="index">
+        <h3>Table of contents</h3>
+        <ol className="index">
+          <li>
+            <a href="#shipping-and-delivery-options">
+              Shipping and Delivery Options
+            </a>
+          </li>
+          <li>
+            <a href="#delayed-orders">Delayed Orders</a>
+          </li>
+          <li>
+            <a href="#returns-and-exchanges">Returns and Exchanges</a>
+          </li>
+          <li>
+            <a href="#contact-information">Contact Information</a>
+          </li>
+        </ol>
+      </div>
+      <h2 id="shipping-and-delivery-options">Shipping and Delivery Options</h2>
+      <p>
+        We offer a variety of shipping options to suit the needs of our
+        customers.
+      </p>
+      <h3>Free Shipping</h3>
+      <p>
+        As part of our commitment to an exceptional shopping experience, we are
+        pleased to offer free shipping.
+      </p>
+      <h3>Flat Rate Shipping</h3>
+      <p>
+        To ensure affordability and simplicity in our shipping process, we
+        provide a flat rate shipping option.
+      </p>
+      <h3>Shipping Methods</h3>
+      <p>
+        We offer a variety of shipping options to suit the needs of our
+        customers:
+      </p>
+      <ul>
+        <li>Standard: 5 to 7 business days</li>
+      </ul>
+      <p>
+        We strive for a swift preparation process and orders are typically
+        processed and dispatched within 1 business day so that customers can
+        receive their items promptly.
+      </p>
+      <p>
+        In certain situations, we may collaborate with a third-party supplier
+        who might handle our inventory and take charge of shipping your
+        products.
+      </p>
+      <h2 id="delayed-orders">Delayed Orders</h2>
+      <p>
+        Unexpected delays can occur due to various reasons such as logistic
+        challenges, inclement weather, high demand, or carrier issues. We are
+        committed to handling these situations with transparency and efficiency.
+        In the event of a delay, our priority is to keep you informed. We will
+        promptly notify you with updates on the status of your order and the
+        expected new delivery time. Our goal is to provide clear and accurate
+        information so you can plan accordingly.
+      </p>
+      <p>
+        Understanding the inconvenience caused by delays, we offer options to
+        maintain your satisfaction. If your order is significantly delayed, you
+        will have the choice to continue with the order, modify it, or cancel it
+        for a full refund. Our customer service team is always available to
+        assist with any changes to your order.
+      </p>
+      <h2 id="returns-and-exchanges">Returns and Exchanges</h2>
+      <p>
+        If you have any questions about refunds, returns or exchanges, please
+        review our{" "}
+        <a href="https://www.dkmondal.in/cancelltion-and-refund">
+          refund policy
+        </a>
+        .
+      </p>
+      <h2 id="contact-information">Contact Information</h2>
+      <p>
+        If you have any questions or concerns regarding our shipping policy, we
+        encourage you to contact us using the details below:
+      </p>
+      <ul>
+        <li>
+          <a href="mailto:debarkamondal@gmail.com">debarkamondal@gmail.com</a>
+        </li>
+      </ul>
+      <p>This document was last updated on May 19, 2025</p>
     </>
   );
 };
